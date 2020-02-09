a9fa755748c34786a4a955709b0351886218d098
Ignacio Serantes <ignacio@aynoa.net>
2020-02-09 22:46:58 +0100

    Fixed path calculation.

--- gittools/vfpgitdiff.bat
+++ gittools/vfpgitdiff.bat
@@ -1,3 +1,4 @@
 @Echo Off
 
-"c:\Program Files\Git\bin\bash.exe" vfpgitdiff %1 %2 %3
+for /f %%i in ('wsl wslpath -a "%1"') do set FILENAME=%%i
+"c:\Program Files\Git\bin\bash.exe" vfpgitdiff %FILENAME:~4% %2 %3
